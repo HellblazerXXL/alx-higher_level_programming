2dd7b2b93e42811cd15e2cc74dda74de3717b58e
HellblazerXXL <royceapera@yahoo.com>
2022-12-05 14:51:19 +0000

    7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word[:3]
 word[7:8]
-word[4:4]
+word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
